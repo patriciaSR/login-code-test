1806d961366abc2a1828dd80447cb1903d0555dc
patriciaSR <patricia.suarez.psr@gmail.com>
2020-07-08 21:52:05 +0200

    test: cobertura formatter

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -11,22 +11,23 @@ module.exports = function (config) {
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-junit-reporter')
+      require('karma-coverage')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/client'),
+      dir: require('path').join(__dirname, './coverage'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml', 'junit'],
-    junitReporter: {
-      outputDir: 'coverage/client',
-      outputFile: 'clover.xml',
-      useBrowserName: false
+    coverageReporter: {
+      dir : 'coverage/',
+      reporters: [
+        { type: 'cobertura', subdir: '.', file: 'clover.xml' },
+      ]
     },
+    reporters: ['progress', 'kjhtml', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
