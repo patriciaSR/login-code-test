d20f6fe0532a299ac5d48e57420e7ffc9c79af87
patriciaSR <patricia.suarez.psr@gmail.com>
2020-07-14 12:06:58 +0200

    test: add logout feature

---
+++ client/e2e/cypress/integration/Logout/logout.js
@@ -0,0 +1,20 @@
+import { And, Given, Then, When } from "cypress-cucumber-preprocessor/steps";
+
+
+Given('Im logged in page', () => {
+  const { email, password } = Cypress.env('userData').user;
+  cy.login(email, password);
+})
+
+And('I open home page', () => {
+  cy.visit('/home');
+})
+
+When('I click on logout button', () => {
+  cy.get('button[type="submit"]').click();
+})
+
+Then('I see login page', () => {
+  cy.url().should('contain', '/login');
+  cy.get('img[alt="karumi-logo"]').should('exist');
+})
